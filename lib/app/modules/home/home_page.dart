0a18bd4bb67fb1629ddc449dd1aa8c1f6c6db312
OcarinhaDaPolo <victor.ifba.info@gmail.com>
2022-08-12 13:03:06 -0300

    Produtos - OK

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:sys_venda/app/core/models/produto_model.dart';
-import 'package:sys_venda/app/core/repositories/product_repository.dart';
+
 import 'package:sys_venda/app/modules/home/home_store.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,30 +26,7 @@ class _HomePageState extends State<HomePage> {
         builder: (context) => Text('${store.counter}'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          bool value = await ProdutoRepository()
-              .updateProduct(6, "Lata de Tinta Bigó - 5L", 100, "L");
-
-          List<ProdutoModel> p = await ProdutoRepository().getAllProducts();
-
-          ProdutoModel pp = await ProdutoRepository().getProductForId(6);
-
-          if (pp.id == 0) {
-            print("vazia fiii");
-          } else {
-            print("tu é foda meu: ${pp.nome}");
-          }
-
-          if (p.isEmpty) {
-            print("vazia fiii");
-          } else {
-            print("tu é foda");
-          }
-
-          if (value) {
-            print("foi foi foi");
-          }
-        },
+        onPressed: () async {},
         child: const Icon(Icons.add),
       ),
     );
